f02a454cca68b4a7372b15e26ae974d694b5a99d
hale22 <hale22dr@gmail.com>
2022-03-15 12:45:19 +0300

    Fixed: project/src/utils.c:5:36: style: Unsigned expression 'i' can't be negative so it is unnecessary to test it. [unsignedPositive]

--- project/src/utils.c
+++ project/src/utils.c
@@ -2,7 +2,7 @@
 
 size_t timer_from(unsigned char from) {
     size_t counter;
-    for (unsigned char i = from; i >= 0; --i) {
+    for (unsigned char i = from; i == 0; --i) {
         ++counter;
         printf(i);
     }
