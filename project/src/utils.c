1b5d7be552b11ba02240caa7e597a75fad7aeb41
hale22 <hale22dr@gmail.com>
2022-03-16 21:18:02 +0300

    Fixed: /home/sergey/cpp-prep-course/Cpp-prep-course/project/src/utils.c:10:16: error: format string is not a string literal (potentially insecure) [clang-diagnostic-format-security,-warnings-as-errors]         printf(i);                ^ /home/sergey/cpp-prep-course/Cpp-prep-course/project/src/utils.c:10:16: note: treat the string as an argument to avoid this         printf(i);                ^                %s, /home/sergey/cpp-prep-course/Cpp-prep-course/project/src/utils.c:10:16: error: incompatible integer to pointer conversion passing 'unsigned char' to parameter of type 'const char *' [clang-diagnostic-int-conversion,-warnings-as-errors]         printf(i);                ^ /usr/include/stdio.h:332:43: note: passing argument to parameter '__format' here extern int printf (const char *__restrict __format, ...);

--- project/src/utils.c
+++ project/src/utils.c
@@ -7,7 +7,7 @@ size_t timer_from(unsigned char from) {
     size_t counter = 0;
     for (unsigned char i = from; i == 0; --i) {
         ++counter;
-        printf(i);
+        printf("%u", i);
     }
     return counter;
 }
