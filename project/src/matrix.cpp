efcb76bc9c0330fd2ee5c5a27306f164e8442f04
hale22 <hale22dr@gmail.com>
2022-07-19 17:23:20 +0300

    Проблема CompareNeg была из-за того, что там стояло исключение DimensionMatch. Я починили исключения, поэтому и тест перестал проходиться.

--- project/src/matrix.cpp
+++ project/src/matrix.cpp
@@ -73,9 +73,6 @@ double& Matrix::operator()(size_t i, size_t j) {
 }
 
 bool Matrix::operator==(const Matrix& rhs) const {
-  if (rows_ != rhs.rows_ || cols_ != rhs.cols_)
-    throw DimensionMismatch(*this, rhs);
-
   for (size_t i = 0; i < rows_; i++) {
     for (size_t j = 0; j < cols_; j++) {
       if (!CompareDouble(matrix_ptr_[i][j], rhs.matrix_ptr_[i][j]))
